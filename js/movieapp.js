67cb46a585fe5d92eb70868ec7cbc1f58c1fd863
Showmmoy Barua <128532264+Showmmoy@users.noreply.github.com>
2026-03-12 06:14:44 +0000

    Movie app

---
+++ js/movieapp.js
@@ -0,0 +1,63 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b594c35e24e362571a3548f49565c890&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=b594c35e24e362571a3548f49565c890&page=1&query="';
+ 
+const main = document.getElementById('main')
+const form =document.getElementById('form')
+const search =document.getElementById('search')
+
+//Get initial movie
+    getMovies(API_URL);
+
+
+async function getMovies(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    showMovies(data.results);
+}
+
+function showMovies(movies) {
+    main.innerHTML = ''
+    movies.forEach((movie) => {
+        const {title, poster_path, vote_average, overview} = movie
+
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+            <img src="${poster_path ? IMG_PATH + poster_path : 'https://via.placeholder.com/1280x720?text=No+Image'}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                <p>${overview}</p>
+            </div>
+        `
+        main.appendChild(movieEl)
+    })
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green'
+    }else if(vote >= 5) {
+        return 'orange'
+    }else {
+        return 'red'
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_URL + searchTerm) 
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+})
